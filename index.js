ffe511ff70288016ae6803a90617e9ad32ffbcdd
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2024-05-29 21:50:25 +0200

    Include handle touch events

--- index.js
+++ index.js
@@ -12,57 +12,135 @@ const secondsAfter = document.querySelectorAll(".seconds-after");
 
 numbersBefore.forEach((time) => (time.style.visibility = time.value < 0 && 'hidden'));
 
-const scrollHours = function(event) {
+// instructions for scroll events
+const scrollEventHours = function(event) {
   const inputHours = document.querySelector(".input-hours");
   if (event.deltaY < 0 && inputHours.value < 24) {
     // Scroll para cima
     inputHours.value++;
-    hourBefore.forEach((hour) => hour.value++);
-    hourAfter.forEach((hour) => hour.value++);
+    hourBefore.forEach((time) => time.value++);
+    hourAfter.forEach((time) => time.value++);
   } else if (event.deltaY > 0 && inputHours.value > 0) {
     // Scroll para baixo
     inputHours.value--;
-    hourBefore.forEach((hour) => hour.value--);
-    hourAfter.forEach((hour) => hour.value--);
+    hourBefore.forEach((time) => time.value--);
+    hourAfter.forEach((time) => time.value--);
   }
-  hourBefore.forEach((hour) => hour.value < 0 ? (hour.style.visibility = 'hidden') : (hour.style.visibility = 'visible'));
-  hourAfter.forEach((hour) => hour.value > 24 ? (hour.style.visibility = 'hidden') : (hour.style.visibility = 'visible'));
+  hourBefore.forEach((time) => time.value < 0 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+  hourAfter.forEach((time) => time.value > 24 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
 }
-const scrollMinutes = function(event) {
+const scrollEventMinutes = function(event) {
   const inputMinutes = document.querySelector(".input-minutes");
   if (event.deltaY < 0 && inputMinutes.value < 60) {
     // Scroll para cima
     inputMinutes.value++;
-    minutesBefore.forEach((hour) => hour.value++);
-    minutesAfter.forEach((hour) => hour.value++);
+    minutesBefore.forEach((time) => time.value++);
+    minutesAfter.forEach((time) => time.value++);
   } else if (event.deltaY > 0 && inputMinutes.value > 0) {
     // Scroll para baixo
     inputMinutes.value--;
-    minutesBefore.forEach((hour) => hour.value--);
-    minutesAfter.forEach((hour) => hour.value--);
+    minutesBefore.forEach((time) => time.value--);
+    minutesAfter.forEach((time) => time.value--);
   }
-  minutesBefore.forEach((hour) => hour.value < 0 ? (hour.style.visibility = 'hidden') : (hour.style.visibility = 'visible'));
-  minutesAfter.forEach((hour) => hour.value > 60 ? (hour.style.visibility = 'hidden') : (hour.style.visibility = 'visible'));
+  minutesBefore.forEach((time) => time.value < 0 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+  minutesAfter.forEach((time) => time.value > 60 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
 }
-const scrollSeconds = function(event) {
+const scrollEventSeconds = function(event) {
   const inputSeconds = document.querySelector(".input-seconds");
   if (event.deltaY < 0 && inputSeconds.value < 60) {
     // Scroll para cima
     inputSeconds.value++;
-    secondsBefore.forEach((hour) => hour.value++);
-    secondsAfter.forEach((hour) => hour.value++);
+    secondsBefore.forEach((time) => time.value++);
+    secondsAfter.forEach((time) => time.value++);
   } else if (event.deltaY > 0 && inputSeconds.value > 0) {
     // Scroll para baixo
     inputSeconds.value--;
-    secondsBefore.forEach((hour) => hour.value--);
-    secondsAfter.forEach((hour) => hour.value--);
+    secondsBefore.forEach((time) => time.value--);
+    secondsAfter.forEach((time) => time.value--);
   }
-  secondsBefore.forEach((hour) => hour.value < 0 ? (hour.style.visibility = 'hidden') : (hour.style.visibility = 'visible'));
-  secondsAfter.forEach((hour) => hour.value > 60 ? (hour.style.visibility = 'hidden') : (hour.style.visibility = 'visible'));
+  secondsBefore.forEach((time) => time.value < 0 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+  secondsAfter.forEach((time) => time.value > 60 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
 }
 
-// Eventos scroll do mouse
-document.querySelector(".container-hours").addEventListener("wheel", scrollHours);
-document.querySelector(".container-minutes").addEventListener("wheel", scrollMinutes);
-document.querySelector(".container-seconds").addEventListener("wheel", scrollSeconds);
+// instructions for touch events
+let startY = 0;
+const touchEventHours = function (event) {
+  const currentY = event.touches[0].clientY;
+  const diffY = currentY - startY;
+  const inputHours = document.querySelector(".input-hours");
+  if (Math.abs(diffY) > 10) {
+    // Detecta um arrasto mínimo de 10 pixels
+    if (diffY < 0 && inputHours.value > 0) {
+      // arrastar para cima aqui
+      inputHours.value--;
+      hourBefore.forEach((time) => time.value--);
+      hourAfter.forEach((time) => time.value--);
+    } else if (inputHours.value < 24) {
+      // arrastar para baixo aqui
+      inputHours.value++;
+      hourBefore.forEach((time) => time.value++);
+      hourAfter.forEach((time) => time.value++);
+    }
+    hourBefore.forEach((time) => time.value < 0 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+    hourAfter.forEach((time) => time.value > 24 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+    startY = currentY;
+  }
+};
+const touchEventMinutes = function (event) {
+  const currentY = event.touches[0].clientY;
+  const diffY = currentY - startY;
+  const inputMinutes = document.querySelector(".input-minutes");
+  if (Math.abs(diffY) > 10) {
+    // Detecta um arrasto mínimo de 10 pixels
+    if (diffY < 0 && inputMinutes.value > 0) {
+      // arrastar para cima aqui
+      inputMinutes.value--;
+      minutesBefore.forEach((time) => time.value--);
+      minutesAfter.forEach((time) => time.value--);
+    } else if (inputMinutes.value < 60) {
+      // arrastar para baixo aqui
+      inputMinutes.value++;
+      minutesBefore.forEach((time) => time.value++);
+      minutesAfter.forEach((time) => time.value++);
+    }
+    minutesBefore.forEach((time) => time.value < 0 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+    minutesAfter.forEach((time) => time.value > 60 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+    startY = currentY;
+  }
+};
+const touchEventSeconds = function (event) {
+  const currentY = event.touches[0].clientY;
+  const diffY = currentY - startY;
+  const inputSeconds = document.querySelector(".input-seconds");
+  if (Math.abs(diffY) > 10) {
+    // Detecta um arrasto mínimo de 10 pixels
+    if (diffY < 0 && inputSeconds.value > 0) {
+      // arrastar para cima aqui
+      inputSeconds.value--;
+      secondsBefore.forEach((time) => time.value--);
+      secondsAfter.forEach((time) => time.value--);
+    } else if (inputSeconds.value < 60) {
+      // arrastar para baixo aqui
+      inputSeconds.value++;
+      secondsBefore.forEach((time) => time.value++);
+      secondsAfter.forEach((time) => time.value++);
+    }
+    secondsBefore.forEach((time) => time.value < 0 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+    secondsAfter.forEach((time) => time.value > 60 ? (time.style.visibility = 'hidden') : (time.style.visibility = 'visible'));
+    startY = currentY;
+  }
+};
 
+const containerHours = document.querySelector(".container-hours");
+const containerMinutes = document.querySelector(".container-minutes");
+const containerSeconds = document.querySelector(".container-seconds");
+
+// scroll events
+containerHours.addEventListener("wheel", scrollEventHours);
+containerMinutes.addEventListener("wheel", scrollEventMinutes);
+containerSeconds.addEventListener("wheel", scrollEventSeconds);
+
+// touch events
+containerHours.addEventListener("touchmove", touchEventHours);
+containerMinutes.addEventListener("touchmove", touchEventMinutes);
+containerSeconds.addEventListener("touchmove", touchEventSeconds);
